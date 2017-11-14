71a706f85c5d7fe51cae8d7894d44d9d3292958c
Francis Boily <boilyfrank28@gmail.com>
2017-11-14 00:59:13 -0500

    Adding the grunt graph command to create contracts graph

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,4 @@
 module.exports = function (grunt) {
-
     // Project configuration.
     grunt.initConfig({
         copy: {
@@ -11,6 +10,8 @@ module.exports = function (grunt) {
             }
         },
         exec: {
+            create_dot_files: buildCommandsForDotFiles(),
+            create_png_graphs: buildCommandsForPngGraphs(),
             local_rpc: 'start testrpc --mnemonic "clog banana trophy city sunset busy citizen biology cash orchard better couch" --accounts 50',
             local_ipfs: 'start ipfs daemon',
             truffle_compile: 'truffle compile',
@@ -28,4 +29,33 @@ module.exports = function (grunt) {
     grunt.registerTask('bootstrap', ['exec:local_rpc', 'exec:local_ipfs']);
     grunt.registerTask('truffle', ['clean', 'exec:truffle_compile', 'exec:truffle_migrate', 'copy:contracts']);
     grunt.registerTask('default', []);
+    grunt.registerTask('graph', ['exec:create_dot_files', 'exec:create_png_graphs']);
 };
+
+function buildCommandsForDotFiles() {
+    const contractsFolder = './contracts/';
+    const fs = require('fs');
+
+    var commands = '';
+    fs.readdir(contractsFolder, (err, files) => {
+        files.forEach(file => {
+            commands += 'solgraph ./contracts/' + file + ' > ./contracts_graph/' + file + '.dot' + ' & '
+        });
+        commands += 'echo DONE';
+        console.log(commands);
+    });
+}
+
+function buildCommandsForPngGraphs() {
+    const contractsGraphFolder = './contracts_graph/';
+    const fs = require('fs');
+
+    var commands = '';
+    fs.readdir(contractsGraphFolder, (err, files) => {
+        files.forEach(file => {
+            commands += 'dot -Tpng ./contracts_graph/' + file + ' > ./contracts_graph/' + file + '.png' + ' & '
+        });
+        commands += 'echo DONE';
+        console.log(commands);
+    });
+}
