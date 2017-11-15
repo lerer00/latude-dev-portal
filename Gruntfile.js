4d1ba24c1f076a30c74623f93c4465f8eb2a4314
Francis Boily <boilyfrank28@gmail.com>
2017-11-14 20:40:28 -0500

    Removing graph since grunt is a pain to setup to setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,6 @@ module.exports = function (grunt) {
             }
         },
         exec: {
-            create_dot_files: buildCommandsForDotFiles(),
-            create_png_graphs: buildCommandsForPngGraphs(),
             local_rpc: 'start testrpc --mnemonic "clog banana trophy city sunset busy citizen biology cash orchard better couch" --accounts 50',
             local_ipfs: 'start ipfs daemon',
             truffle_compile: 'truffle compile',
@@ -25,37 +23,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-exec');
 
-    // Task definitions.
+    // Tasks.
     grunt.registerTask('bootstrap', ['exec:local_rpc', 'exec:local_ipfs']);
     grunt.registerTask('truffle', ['clean', 'exec:truffle_compile', 'exec:truffle_migrate', 'copy:contracts']);
     grunt.registerTask('default', []);
-    grunt.registerTask('graph', ['exec:create_dot_files', 'exec:create_png_graphs']);
 };
-
-function buildCommandsForDotFiles() {
-    const contractsFolder = './contracts/';
-    const fs = require('fs');
-
-    var commands = '';
-    fs.readdir(contractsFolder, (err, files) => {
-        files.forEach(file => {
-            commands += 'solgraph ./contracts/' + file + ' > ./contracts_graph/' + file + '.dot' + ' & '
-        });
-        commands += 'echo DONE';
-        console.log(commands);
-    });
-}
-
-function buildCommandsForPngGraphs() {
-    const contractsGraphFolder = './contracts_graph/';
-    const fs = require('fs');
-
-    var commands = '';
-    fs.readdir(contractsGraphFolder, (err, files) => {
-        files.forEach(file => {
-            commands += 'dot -Tpng ./contracts_graph/' + file + ' > ./contracts_graph/' + file + '.png' + ' & '
-        });
-        commands += 'echo DONE';
-        console.log(commands);
-    });
-}
